ce4483baaff959feb71a149b02b952515e8bf5cf
Drodriguez87 <drodriguezal@soy.sena.edu.co>
2023-10-22 12:30:27 -0500

    Subiendo archivos

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,14 +1,29 @@
 <?php
     class DataBase{
         public static function connection(){            
-            $hostname = "localhost";
+            $hostname = "mitra.mysql.database.azure.com";
             $port = "3306";
             $database = "db_mitra";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
-			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			return $pdo;
-		}
-	}
+            $username = "David";
+            $password = "Drodriguez12@";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
+            );
+            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        }
+}
+    // class DataBase{
+    //     public static function connection(){            
+    //         $hostname = "localhost";
+    //         $port = "3306";
+    //         $database = "db_mitra";
+    //         $username = "root";
+    //         $password = "";
+	// 		$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+	// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 		return $pdo;
+	// 	}
+	// }
 ?>
